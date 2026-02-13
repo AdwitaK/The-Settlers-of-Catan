fef74b003608301b537421b99eebd90424b8a169
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-02-13 18:41:06 -0500

    changed map layout to use different map (#7)

--- src/MapLayout.java
+++ src/MapLayout.java
@@ -1,14 +1,14 @@
 package src;
 
 public class MapLayout {
-    public static final int [] tokens = {0, 4, 3, 11, 6, 4, 3, 11, 6, 5, 8, 9, 12, 10, 2, 9, 10, 8, 5};
+    public static final int [] tokens = {10, 11, 8, 3, 11, 5, 12, 3, 6, 4, 6, 9, 5, 9, 8, 4, 7, 2, 10};
     public static final ResourceType [] tileResource = {
-        ResourceType.DESERT, ResourceType.GRAIN, ResourceType.ORE, 
-        ResourceType.LUMBER, ResourceType.BRICK, ResourceType.WOOL,
-        ResourceType.LUMBER, ResourceType.WOOL, ResourceType.GRAIN,
-        ResourceType.BRICK, ResourceType.LUMBER, ResourceType.GRAIN,
-        ResourceType.GRAIN, ResourceType.ORE, ResourceType.WOOL, 
-        ResourceType.LUMBER, ResourceType.BRICK, ResourceType.ORE,
+        ResourceType.LUMBER, ResourceType.GRAIN, ResourceType.BRICK, 
+        ResourceType.ORE, ResourceType.WOOL, ResourceType.WOOL,
+        ResourceType.WOOL, ResourceType.GRAIN, ResourceType.ORE,
+        ResourceType.LUMBER, ResourceType.ORE, ResourceType.GRAIN,
+        ResourceType.LUMBER, ResourceType.BRICK, ResourceType.BRICK, 
+        ResourceType.GRAIN, ResourceType.DESERT, ResourceType.LUMBER,
         ResourceType.WOOL
     };
     
